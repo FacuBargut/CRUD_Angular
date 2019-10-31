123d23a3a30f5c47c4ca07438b7ebbde5f4b6770
= <=>
2019-10-31 17:35:24 -0300

    DELETE- para eliminar registros

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-heroes',
@@ -18,6 +19,26 @@ export class HeroesComponent implements OnInit {
     this.heroesService.getHeroes()
         .subscribe( 
         resp=> this.heroes = resp )
+  }
+
+  borrarHeroe( heroe: HeroeModel, i:number){
+
+    Swal.fire({
+      title: 'Está seguro?',
+      text: `Está seguro que desea borrar a ${ heroe.nombre}`,
+      type: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then( resp => {
+        if(resp.value){
+          this.heroes.splice(i,1);
+          this.heroesService.borrarHeroe(heroe.id).subscribe();
+        }
+    });
+
+
+
+
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -46,6 +46,12 @@ export class HeroesService {
            );
   }
 
+  borrarHeroe( id:string){
+
+    return this.http.delete(`${ this.url }/heroes/${ id }.json`)
+
+  }
+
   getHeroe( id:string ){
       return this.http.get(`${ this.url }/heroes/${ id }.json`)
   }
